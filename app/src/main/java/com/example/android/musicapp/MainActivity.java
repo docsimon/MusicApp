6bef09fe7e38c9c4fb5476cebd943f1840779a2f
doc <sialtek@gmail.com>
2017-08-10 16:16:21 +0100

    Implemented Data Binding and back button

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.musicapp.databinding.ActivityMainBinding;
+
+import static android.R.attr.button;
+import static android.R.id.message;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding myBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        myBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+        Button searcButton = myBinding.searchButton;
+        searcButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent searchResultIntent = new Intent(MainActivity.this, ListResultsActivity.class);
+
+                // Start the new activity
+                startActivity(searchResultIntent);
+            }
+        });
+
     }
 }

65909453c2a819d2a8724eeb449433716345242f
doc <sialtek@gmail.com>
2017-08-10 17:14:08 +0100

    Passing the search term to the intent

--- app/src/main/java/com/example/android/musicapp/ListResultsActivity.java
+++ app/src/main/java/com/example/android/musicapp/ListResultsActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.musicapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.example.android.musicapp.databinding.ActivityListResultsBinding;
+
 
 public class ListResultsActivity extends AppCompatActivity {
+
+    ActivityListResultsBinding myBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_results);
+        myBinding = DataBindingUtil.setContentView(this, R.layout.activity_list_results);
+
+        String s = getIntent().getStringExtra("Result Title");
+
+
+        TextView mainTitle = myBinding.searchMainTitle;
+        String cippa =  getResources().getString(R.string.main_title_search, s);
+
+        mainTitle.setText(cippa);
+
+
     }
 }
--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,7 +15,6 @@ import com.example.android.musicapp.databinding.ActivityMainBinding;
 
 import static android.R.attr.button;
 import static android.R.id.message;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,19 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         myBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-
         Button searcButton = myBinding.searchButton;
         searcButton.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the numbers category is clicked on.
             @Override
             public void onClick(View view) {
                 // Create a new intent to open the {@link NumbersActivity}
-                Intent searchResultIntent = new Intent(MainActivity.this, ListResultsActivity.class);
 
-                // Start the new activity
-                startActivity(searchResultIntent);
+                // get the reference and content of the edit
+                EditText searchField = myBinding.searchField;
+                String query = searchField.getText().toString();
+                if (query.isEmpty()){
+                    displayAlertMsg();
+                }else {
+                    // define the intent
+                    Intent searchResultIntent = new Intent(MainActivity.this, ListResultsActivity.class);
+                    // passing the search query to the intent
+                    searchResultIntent.putExtra("Result Title", query);
+                    // Start the new activity
+                    startActivity(searchResultIntent);
+                }
             }
         });
+    }
 
+    private void displayAlertMsg(){
+        String message = getResources().getString(R.string.search_empty);
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }

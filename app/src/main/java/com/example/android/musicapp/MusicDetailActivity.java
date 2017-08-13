a52694a48c4e4953550decd22e02dba6154a793d
doc <sialtek@gmail.com>
2017-08-13 22:31:48 +0100

    creating the music detail page

---
+++ app/src/main/java/com/example/android/musicapp/MusicDetailActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.musicapp;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.musicapp.databinding.ActivityMusicDetailBinding;
+
+public class MusicDetailActivity extends AppCompatActivity {
+
+    ActivityMusicDetailBinding myBinding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myBinding = DataBindingUtil.setContentView(this, R.layout.activity_music_detail);
+
+        String s = getIntent().getStringExtra("Result Title");
+
+
+        TextView mainTitle = myBinding.searchMainTitle;
+        String cippa =  getResources().getString(R.string.main_title_search, s);
+
+        mainTitle.setText(cippa);
+
+
+    }
+
+}
